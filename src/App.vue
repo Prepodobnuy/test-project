abca17f38c071cb26c72ed1a4cb9171aae3fcc06
prepodobnuy <zvfvbycsy6@gmail.com>
2024-10-22 18:32:50 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import MainHeader from '@/components/MainHeader.vue'
+</script>
+
+<template>
+  <MainHeader />
+  <RouterView />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <RouterLink class="home-href">
+      ikakprosto.ru
+    </RouterLink>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 76px;
+
+  .home-href {
+    color: var(--color-accent-primary);
+    font-family: "SF Pro Display";
+    cursor: pointer;
+    font-size: 40px;
+    font-weight: bold;
+    text-decoration: none;
+    user-select: none;
+  }
+}
+</style>
